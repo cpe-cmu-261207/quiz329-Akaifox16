783a614b88eb740434c9b2ea910e590dda8440d0
Akaifox16 <54855870+Akaifox16@users.noreply.github.com>
2021-02-09 14:04:24 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { useState, useEffect } from "react";
 
 function App() {
   //add useState for all state variables
-
+  const [input, setInput] = useState({ name: "", gender: "", age: 0 });
+  const [items, setItems] = useState([]);
   //load locationStorage
   useEffect(() => {
-    const items = localStorage.getItem("items");
+    const item = localStorage.getItem("items");
     // ...
+    if (item) {
+      setItems(JSON.parse(item));
+    }
   }, []);
+  useEffect(() => {
+    console.log(items);
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   return (
     <div className="card" style={{ width: 400 }}>
@@ -20,27 +28,51 @@ function App() {
             className="input"
             type="text"
             placeholder="e.q John Smith"
+            onChange={(e) => {
+              setInput({ ...input, name: e.currentTarget.value });
+            }}
             //update related state based on event
           ></input>
         </div>
 
         <div className="field">
           <label className="label">Gender</label>
-          <select className="input" type="text" placeholder="Please select ..">
+          <select
+            className="input"
+            type="text"
+            placeholder="Please select .."
+            onChange={(e) => {
+              setInput({ ...input, gender: e.currentTarget.value });
+            }}
+          >
             <option value="" disabled selected hidden>
               -- Select Gender --
             </option>
-            <option>Male</option>
-            <option>Female</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
           </select>
         </div>
 
         <div className="field">
           <label className="label">Age</label>
-          <input className="input" type="number" placeholder="e.q 30"></input>
+          <input
+            className="input"
+            type="number"
+            placeholder="e.q 30"
+            onChange={(e) => {
+              setInput({ ...input, age: e.currentTarget.value });
+            }}
+          ></input>
         </div>
 
-        <button className="button is-primary is-fullwidth">Submit</button>
+        <button
+          className="button is-primary is-fullwidth"
+          onClick={() => {
+            setItems([...items, input]);
+          }}
+        >
+          Submit
+        </button>
 
         <div className="mb-4"></div>
 
@@ -48,7 +80,12 @@ function App() {
         <p className="is-4 title has-text-centered">Person List</p>
         {/* sample table */}
         <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
-        <p>Your name and code here</p>
+        {items.map((item) => {
+          return (
+            <ItemTable name={item.name} gender={item.gender} age={item.age} />
+          );
+        })}
+        <p>Kamonpat Sunthonpong 620610771</p>
       </div>
     </div>
   );
